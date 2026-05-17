ca43224b3d73e05b4c59efb9b156b770ea4495d6
loukasd <drossosl@gmail.com>
2026-05-17 22:31:39 +0300

    Initial React portfolio setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="app">
+      <h1>Portfolio website started</h1>
+      <p>If you see this, React is working correctly.</p>
+    </div>
+  )
+}
+
+export default App
